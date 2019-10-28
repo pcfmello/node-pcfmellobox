c97c4dadcba3465392449acfabaebd9b0791b87d
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-10-28 17:13:56 -0300

    Creating schemas of application

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const routes = require('./routes');
-const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://pcfmello:ciclismo@cluster0-3fpe5.mongodb.net/node_omni_project_dropbox?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const routes = require('./routes');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
